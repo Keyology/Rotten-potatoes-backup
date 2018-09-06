b3b4eaa7216b171f2a17305368005f073b9543ad
Keoni Murray <keonimurray45@gmail.com>
2018-09-06 14:45:11 -0700

    Added MongoDB and Review model

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
 let exphbs = require('express-handlebars');
-
-app.get('/', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', {useMongoClient: true})
+const Review = mongoose.model('Review', {
+  title: String
 });
-let reviews = [{title:"Great Review"}, {title:"Next Review"}, {title: "Awsome movie"}, {title: "Great film"}
-];
+app.get('/', (req, res) => {
+  Review.find().then(reviews => {
+    res.render('reviews-index', { reviews: reviews });
 
+  })
+
+});
+/*let reviews = [{title:"Great Review"}, {title:"Next Review"}, {title: "Awsome movie"}, {title: "Great film"}
+];*/
+app.get('/', (req, res) => {
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+});
+Review.find().then((review) => {
+  // Code in here is executed when the promise resolves
+});
 app.get('/reviews', (req,res)=>{res.render('reviews-index', { reviews: reviews });})
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
