d045da29918dd7160712c5d8f8d2e8d79b4ff79d
Keoni Murray <keonimurray45@gmail.com>
2018-09-13 10:00:04 -0700

    new update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+const comments = require('./controllers/comments')
 
 const reviews = require('./controllers/reviews');
 const exphbs = require('express-handlebars');
